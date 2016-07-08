2aaa7dbaa4630ba92ce887add15f2b43a4f6f0f1
jhodges10-OSX <sanvidpro@provhinjay.local>
2016-07-07 18:19:14 -0700

    Added some lunch picking logic
    Still issues regarding the classification of the message type. Not too
    hard to fix though.
    
    The bigger problem is that when you send_to_slack you force the
    slack_rtm connection in the other thread to disconnect for a second.

--- encode_bot_combined.py
+++ encode_bot_combined.py
@@ -6,11 +6,11 @@ import sandwich
 authy = sandwich.get_auth('/Volumes/Sandwich/assets/python/auth.csv')
 token = authy['slack']['token']
 sc = SlackClient(token)
-#chan = "C06PTC83B" #LIVE OUTPUT
-chan = "C12PL8TSS" #TEMP SLACK
+#chan = 'C06PTC83B' #LIVE OUTPUT
+chan = 'C12PL8TSS' #TEMP SLACK
 
 message_queue = Queue.Queue()
-lunch_option = ["Fritzi","Grow","Edibol","Cafe Gratitude","Pie Hole","Wurstkurch","Cerveteca","Americano","Umami","Groundwork",]
+lunch_options = ['Fritzi','Grow','Edibol','Cafe Gratitude','Pie Hole','Wurstkurch','Cerveteca','Americano','Umami','Groundwork',]
 
 
 class User:
@@ -20,21 +20,21 @@ class User:
         self.clip_pref = clip_pref
         try:
             user_info = sc.api_call('users.info', user=UUID)
-            fn = user_info["first_name"]
-            ln = user_info["last_name"]
+            fn = user_info['first_name']
+            ln = user_info['last_name']
             full_name = fn + ' ' + ln
         except:
-            full_name = "encoder"
+            full_name = 'encoder'
         self.display_name = full_name
         
 	def print_user_info(self):
-		print "Name: ", self.display_name, ", UUID:", self.UUID
+		print 'Name: ', self.display_name, ', UUID: ', self.UUID
 
 def send_to_slack(slackput):
     try:
         sc.rtm_send_message(chan,slackput)
     except:
-        print "Something happened while attempting to send your message to slack"
+        print 'Something happened while attempting to send your message to slack'
 
 def listen_to_slack():
     evt = ''
@@ -45,17 +45,17 @@ def listen_to_slack():
                 while True:
                     new_evts = sc.rtm_read()
                     for evt in new_evts:
-                        if "type" in evt:
-                            if evt["type"] == "message" and "text" in evt:
-                                message = evt["text"]
-                                person = evt["user"]
+                        if 'type' in evt:
+                            if evt['type'] == 'message' and 'text' in evt:
+                                message = evt['text']
+                                person = evt['user']
                                 output_command = [person,message]
                                 message_queue.put(output_command)
                                 time.sleep(1)
             else:
-                print "Unable to connect to Slack right now"
+                print 'Unable to connect to Slack right now'
         except:
-            print "You caused a much larger problem than you probably realize."
+            print 'You caused a much larger problem than you probably realize.'
 
 def listener():
     listener = threading.Thread(target=listen_to_slack)
@@ -64,14 +64,18 @@ def listener():
 
 
 def check_message_type(cur_message):
-    if "cuts" in cur_message and "scenes" not in cur_message:
+    if 'cuts' in cur_message and 'scenes' not in cur_message:
         return 0
-    elif "/Volumes/" in cur_message:
+    elif '/Volumes/' in cur_message:
         return 1
-    elif "lunch" or "food" in cur_message:
+    elif 'lunch' or 'food' in cur_message:
         return 2
     else:
         return 3
+
+def lunch_answer():
+    lunch_message = 'You should have ' +str(lunch_options[random.randint(0,9)]) + ' for lunch.'
+    send_to_slack(lunch_message)
 
 def conversation():
     listener()
@@ -79,12 +83,9 @@ def conversation():
         cur_message = message_queue.get()
         time.sleep(1)
         print cur_message
-        if check_message_type(cur_message) <= 1:
-            print "this is a cut or scene"
-        elif check_message_type(cur_message) == 2:
-            lunch_message = "You should have " +str(lunch_option[random.randint(0,9)]) + " for lunch."
-            send_to_slack(lunch_message)
-        user1 = User(cur_message[0],cur_message[1], '', '')
-#        User.print_user_info(user1)
-        
+        if check_message_type(cur_message[1]) <= 1:
+            print 'This is either a cut or a scene.'
+        elif check_message_type(cur_message[1]) == 2:
+            lunch_answer()
+            
 conversation()
